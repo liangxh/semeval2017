aa2be28e6cc2e63f3558a9987c6068bdac762de0
XWCHAO <wenwen0950@163.com>
2016-11-14 19:46:21 +0800

    update cnn.py

--- cnn.py
+++ cnn.py
@@ -31,32 +31,35 @@ hidden_dims = 250
 nb_epoch = 20
 
 print('Loading data...')
-key_subtask = 'C'
+key_subtask = 'B'
 vocabs = data_manager.read_vocab_topN(key_subtask, max_features)
 
+text_indexer = input_adapter.get_text_indexer(vocabs)
+label_indexer = input_adapter.get_label_indexer(key_subtask)
+
 dataset = []
-for mode in ['train', 'dev']:
+for mode in ['train', 'dev', 'devtest']:
     texts_labels = data_manager.read_texts_labels(key_subtask, mode)
-
-    text_indexer = input_adapter.get_text_indexer(vocabs)
-    label_indexer = input_adapter.get_label_indexer(key_subtask)
-
     x, y = input_adapter.adapt_texts_labels(texts_labels, text_indexer, label_indexer)
     dataset.append((x, y))
 
 dataset = tuple(dataset)  # list of 2 tuples --> tuple of tuple
-train, dev = dataset
+train, dev, devtest = dataset
 X_train, y_train = train
-X_test, y_test = dev
+X_dev, y_dev = dev
+X_test, y_test = devtest
 
 
 print(len(X_train), 'train sequences')
+print(len(X_dev), 'development sequences')
 print(len(X_test), 'test sequences')
 
 print('Pad sequences (samples x time)')
 X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+X_dev = sequence.pad_sequences(X_dev, maxlen=maxlen)
 X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
 print('X_train shape:', X_train.shape)
+print('X_dev shape:', X_dev.shape)
 print('X_test shape:', X_test.shape)
 
 print('Build model...')
@@ -88,10 +91,17 @@ print(model.output_shape)
 # We project onto a single unit output layer, and squash it with a sigmoid:
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
+
+
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 model.fit(X_train, y_train,
           batch_size=batch_size,
           nb_epoch=nb_epoch,
-          validation_data=(X_test, y_test))
+          validation_data=(X_dev, y_dev))
+
+score, acc = model.evaluate(X_test, y_test,
+                            batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)  # 0.795589
