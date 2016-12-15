a355afd16c25f6b5f2c5ff87c3e799ee22916237
XWCHAO <wenwen0950@163.com>
2016-12-15 09:21:44 +0800

    add pretrain_CNN+bgrnn+blstm.py

--- pretrain_finki.py
+++ pretrain_finki.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 """
 @author: xiwen zhao
-@created: 2016.12.12
+@created: 2016.12.13
 """
 
 from optparse import OptionParser
-from trainer import BaseTrainer
+from pre_trainer import BaseTrainer
 from common import data_manager
 
 from keras.models import Sequential
@@ -28,13 +28,13 @@ class Trainer(BaseTrainer):
 
     def set_model_config(self, options):
         self.config = dict(
-            nb_filter = options.nb_filter,
+            nb_filter_pre = options.nb_filter_pre,
             filter_length = options.filter_length,
             hidden_dims = options.hidden_dims,
             dropout_W = options.dropout_W,
             dropout_U = options.dropout_U,
             optimizer = options.optimizer,
-            rnn_output_dims = options.rnn_output_dims
+            rnn_output_dims_pre = options.rnn_output_dims_pre
         )
 
     def get_optimizer(self, key_optimizer):
@@ -51,7 +51,7 @@ class Trainer(BaseTrainer):
                                 weights=[weights['Wemb']] if 'Wemb' in weights else None),
                                 # dropout=0.2,
                                 )
-        gru_model.add(GRU(config['rnn_output_dims'], dropout_W=config['dropout_W'], dropout_U=config['dropout_U']))
+        gru_model.add(GRU(config['rnn_output_dims_pre'], dropout_W=config['dropout_W'], dropout_U=config['dropout_U']))
 
         cnn_model = Sequential()
         cnn_model.add(Embedding(config['max_features'],
@@ -62,7 +62,7 @@ class Trainer(BaseTrainer):
                                 )
 
         # cnn_model.add(ZeroPadding1D(int(config['filter_length'] / 2)))
-        cnn_model.add(Convolution1D(nb_filter=config['nb_filter'],
+        cnn_model.add(Convolution1D(nb_filter=config['nb_filter_pre'],
                                     filter_length=config['filter_length'],
                                     border_mode='valid',
                                     activation='relu',
@@ -83,61 +83,14 @@ class Trainer(BaseTrainer):
 
         return merged_model
 
-    def build_model(self, config, weights):
-        gru_model = Sequential()
-        gru_model.add(Embedding(config['max_features'],
-                                config['embedding_dims'],
-                                input_length=config['input_length'],
-                                weights=[weights['Wemb']] if 'Wemb' in weights else None),
-                                # dropout=0.2,
-                                )
-        gru_model.add(GRU(config['rnn_output_dims'], dropout_W=config['dropout_W'], dropout_U=config['dropout_U']))
-
-        cnn_model = Sequential()
-        cnn_model.add(Embedding(config['max_features'],
-                                config['embedding_dims'],
-                                input_length = config['input_length'],
-                                weights = [weights['Wemb']] if 'Wemb' in weights else None),
-                                # dropout = 0.2
-                                )
-
-        # cnn_model.add(ZeroPadding1D(int(config['filter_length'] / 2)))
-        cnn_model.add(Convolution1D(nb_filter=config['nb_filter'],
-                                    filter_length=config['filter_length'],
-                                    border_mode='valid',
-                                    activation='relu',
-                                    subsample_length=1))
-
-        cnn_model.add(GlobalMaxPooling1D())
-
-        # merged model
-        merged_model = Sequential()
-        merged_model.add(Merge([gru_model, cnn_model], mode='concat', concat_axis=1))
-
-        merged_model.add(Dropout(0.25))
-
-        if config['nb_classes'] > 2:
-            merged_model.add(Dense(config['nb_classes'], activation='softmax'))
-            loss_type = 'categorical_crossentropy'
-        else:
-            merged_model.add(Dense(1, activation='sigmoid'))
-            loss_type = 'binary_crossentropy'
-
-        merged_model.compile(loss=loss_type,
-                             optimizer=self.get_optimizer(config['optimizer']),
-                             metrics=['accuracy'])
-
-        return merged_model
-
 
 def main():
     optparser = OptionParser()
     optparser.add_option("-t", "--task", dest="key_subtask", default="D")
     optparser.add_option("-p", "--nb_epoch", dest="nb_epoch", type="int", default=50)
     optparser.add_option("-e", "--embedding", dest="fname_Wemb", default="glove.twitter.27B.25d.txt.trim")
-    optparser.add_option("-d", "--hidden_dims", dest="hidden_dims", type="int", default=250)
-    optparser.add_option("-f", "--nb_filter", dest="nb_filter", type="int", default=100)
-    optparser.add_option("-r", "--rnn_output_dims", dest="rnn_output_dims", type="int", default=100)
+    optparser.add_option("-f", "--nb_filter_pre", dest="nb_filter_pre", type="int", default=1024)
+    optparser.add_option("-r", "--rnn_output_dims_pre", dest="rnn_output_dims_pre", type="int", default=1024)
     optparser.add_option("-l", "--filter_length", dest="filter_length", type="int", default=3)
     optparser.add_option("-w", "--dropout_W", dest="dropout_W", type="float", default=0.25)
     optparser.add_option("-u", "--dropout_U", dest="dropout_U", type="float", default=0.25)
@@ -146,22 +99,6 @@ def main():
 
     trainer = Trainer(opts)
     trainer.pre_train()
-
-    trainer.load_model_weight()
-    trainer.train()
-
-    score = trainer.evaluate('test_new', verbose=1)
-    print "Evaluation score: %.3f" % score
-
-    '''
-    test = data_manager.read_texts_labels(opts.key_subtask, 'devtest')
-    loss, acc = trainer.simple_evaluate(test)
-    print "Loss: %.3f Acc: %.3f Evaluation score: %.3f" % (loss, acc, trainer.evaluate(test))
-
-    trainer.load_model_weight()
-    loss, acc = trainer.simple_evaluate(test)
-    print "Loss: %.3f Acc: %.3f Evaluation score: %.3f" % (loss, acc, trainer.evaluate(test))
-    '''
 
 if __name__ == '__main__':
     main()
